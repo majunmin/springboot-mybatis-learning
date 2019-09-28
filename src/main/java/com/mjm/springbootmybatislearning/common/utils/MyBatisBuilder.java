2421654f1a49b2388aa590f26fc6700251214641
majm <majm@ushareit.com>
2019-09-29 00:21:18 +0800

    [learning][spring-mybatis]  加载资源文件

--- src/main/java/com/mjm/springbootmybatislearning/common/utils/MyBatisBuilder.java
+++ src/main/java/com/mjm/springbootmybatislearning/common/utils/MyBatisBuilder.java
@@ -1,5 +1,6 @@
 package com.mjm.springbootmybatislearning.common.utils;
 
+import org.apache.ibatis.io.Resources;
 import org.mybatis.generator.api.MyBatisGenerator;
 import org.mybatis.generator.config.Configuration;
 import org.mybatis.generator.config.xml.ConfigurationParser;
@@ -7,8 +8,8 @@ import org.mybatis.generator.exception.InvalidConfigurationException;
 import org.mybatis.generator.exception.XMLParserException;
 import org.mybatis.generator.internal.DefaultShellCallback;
 
-import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,15 +17,21 @@ import java.util.List;
 public class MyBatisBuilder {
 
     public static void main(String[] args) {
-        String path = MyBatisBuilder.class.getClass().getResource("/").getPath() + "mybatis-generator/generatorConfig"
-                + ".xml";
+
+        InputStream inputStream = null;
+
+        try {
+            inputStream = Resources.getResourceAsStream("mybatis-generator/generatorConfig.xml");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         List<String> warnings = new ArrayList<String>();
         boolean overwrite = true;
-        File configFile = new File(path);
         ConfigurationParser cp = new ConfigurationParser(warnings);
         Configuration config = null;
+
         try {
-            config = cp.parseConfiguration(configFile);
+            config = cp.parseConfiguration(inputStream);
             DefaultShellCallback callback = new DefaultShellCallback(overwrite);
             MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
             myBatisGenerator.generate(null);
